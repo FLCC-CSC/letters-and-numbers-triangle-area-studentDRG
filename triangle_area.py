b07e0ed02b5a43e53f987d24793a9d92c5da7ba1
studentDRG <info@oscqr.com>
2025-02-16 21:33:25 -0500

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -1,79 +1,20 @@
-# FILE NAME - triangle_area.py
+# FILE NAME - triangleArea.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME - 
+# DATE - 
+# DESCRIPTION - 
 
+def main():
+    findArea()
 
+def findArea():
+    height = int(input('Enter the height: '))
+    base = int(input('Enter the base: '))
 
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions below
-# 4. The Sample Output has been included in this code for your convenience
-
-
-
-
-
-########## ENTER YER CODE BELOW THIS LINE ##########
+    print()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-########### END YER CODE ABOVE THIS LINE ###########
+    area = height * base / 2
 
+    print('The area of the triangle is', area)
 
-
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-
-Enter the height: 1
-Enter the base: 1
-
-The area of the triangle is 0.5
-
-'''
-
-
-
-'''
-
-Enter the height: 8
-Enter the base: 4
-
-The area of the triangle is 16.0
-
-'''
-
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. What is the flow of the program? Which line of code kicks off the process?
-
-
-
-
-
-2. What was the hardest part of this lab?
-
-
-
-
-
-'''
+main()
